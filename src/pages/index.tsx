e16fdac8321adbe887fb1849355e753231deb652
tmk <morimoto.ke@gmail.com>
2022-01-02 18:35:09 +0900

    ✨ TOPページにもリンク追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,39 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div className="w-full">
-      <p className="text-4xl">Nextjsで学ぶReact講座</p>
-      <p>JSONPlaceholderのAPIを色々叩いてみるよ</p>
+      <p className="text-4xl">API確認用</p>
+      <p>JSONPlaceholderのAPIで練習</p>
+
+      <ul className="list-inside list-disc mt-2 text-xl">
+        <li className="hover:text-blue-500">
+          <Link href="/">
+            <a>TOP</a>
+          </Link>
+        </li>
+        <li className="hover:text-blue-500">
+          <Link href="/posts">
+            <a>POSTS</a>
+          </Link>
+        </li>
+        <li className="hover:text-blue-500">
+          <Link href="/users">
+            <a>Users</a>
+          </Link>
+        </li>
+        <li className="hover:text-blue-500">
+          <Link href="/comments">
+            <a>Comments</a>
+          </Link>
+        </li>
+        <li className="hover:text-blue-500">
+          <Link href="/ssrcomments">
+            <a>SSRCommonents</a>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
